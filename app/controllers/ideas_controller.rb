908fffd5462a22ff664bccbc906f6f461591d4d1
Nalicyclica <siruri0xf3@gmail.com>
2021-09-19 17:10:07 +0900

    createアクションの作成　save処理の追加

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,18 @@
 class IdeasController < ApplicationController
   def create
+    categoryIdea = CategoryIdea.new(idea_params)
+    if categoryIdea.save
+      render status: 201, json: categoryIdea
+    else
+      render status: 422, json: categoryIdea.errors
+    end
+  end
+
+  private
+
+  def idea_params
+    params_keys = [:category_name, :body]
+    permit_params = params.permit(params_keys)
+    return {name: permit_params[:category_name], body: permit_params[:body]}
   end
 end
--- app/models/category_idea.rb
+++ app/models/category_idea.rb
@@ -1,10 +1,31 @@
 class CategoryIdea
   include ActiveModel::Model
-  attr_accessor :name, :body, :category_id
+  attr_accessor :name, :body
 
   with_options presence: true do
-    validates :name, uniqueness: true
+    validates :name
     validates :body
-    validates :category_id
+  end
+
+  def save
+    return false if !valid?
+    category = category_exist_or_not()
+    category = Category.create(name: name) if !category
+    idea_create(category.id)
+  end
+  
+  private
+
+  def category_exist_or_not
+    return Category.find_by(name: name)
+  end
+  
+  def idea_create(category_id)
+    if Idea.create(body: body, category_id: category_id)
+      return true
+    else
+      errors.add(:base, "不正なカテゴリーです")
+      return false
+    end
   end
 end
