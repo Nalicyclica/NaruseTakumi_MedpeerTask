2b2498466d90d50e370fb95ca5d2fc6c488ddcde
Nalicyclica <siruri0xf3@gmail.com>
2021-09-21 18:03:32 +0900

    テストコードの修正

--- spec/requests/ideas_spec.rb
+++ spec/requests/ideas_spec.rb
@@ -3,48 +3,43 @@ require 'rails_helper'
 RSpec.describe 'Ideas', type: :request do
   describe 'GET #index' do
     before do
-      @item_number = 5
-      @categories = []
-      @ideas = []
-      @item_number.times do |i|
-        category = Category.create(name: "rpsec_test_#{i}")
-        @categories << category
-        @item_number.times do |j|
-          @ideas << FactoryBot.create(:idea, category_id: category.id)
-        end
-      end 
-      @expected_data = {
-        'id' => @ideas[0].id,
-        'category' => @categories[0].name,
-        'body' => @ideas[0].body,
-        'created_at' => @ideas[0].created_at.to_i
+      @first_idea = FactoryBot.create(:idea)
+      @second_idea = FactoryBot.create(:idea)
+      @first_idea_response_data = {
+        'id' => @first_idea.id,
+        'category' => @first_idea.category.name,
+        'body' => @first_idea.body,
+        'created_at' => @first_idea.created_at.to_i
       }
-      @unexpected_data = {
-        'id' => @ideas[@item_number].id,
-        'category' => @categories[1].name,
-        'body' => @ideas[@item_number].body,
-        'created_at' => @ideas[@item_number].created_at.to_i
+      @second_idea_response_data = {
+        'id' => @second_idea.id,
+        'category' => @second_idea.category.name,
+        'body' => @second_idea.body,
+        'created_at' => @second_idea.created_at.to_i
       }
     end
     context '正常なレスポンス' do
       it 'category_nameを指定しないと、全てのideaを取得できる' do
         get ideas_path, params: {}
         data = JSON.parse(response.body)["data"]
-        expect(data.length).to eq @item_number * @item_number
-        expect(data).to include(@expected_data)
+        expect(response.status).to eq 200
+        expect(data.length).to eq 2
+        expect(data).to include(@first_idea_response_data)
       end
       it 'category_nameが空欄だと、全てのideaを取得できる' do
         get ideas_path, params: {category_name: ""}
         data = JSON.parse(response.body)["data"]
-        expect(data.length).to eq @item_number * @item_number
-        expect(data).to include(@expected_data)
+        expect(response.status).to eq 200
+        expect(data.length).to eq 2
+        expect(data).to include(@first_idea_response_data)
       end
       it 'category_nameを指定すると、そのカテゴリーのideaを取得できる' do
-        get ideas_path, params: {category_name: @categories[0].name}
+        get ideas_path, params: {category_name: @first_idea.category.name}
         data = JSON.parse(response.body)["data"]
-        expect(data.length).to eq @item_number
-        expect(data).to include(@expected_data)
-        expect(data).to_not include(@unexpected_data)
+        expect(response.status).to eq 200
+        expect(data.length).to eq 1
+        expect(data).to include(@first_idea_response_data)
+        expect(data).to_not include(@second_idea_response_data)
       end
     end
     context 'エラーレスポンス' do
